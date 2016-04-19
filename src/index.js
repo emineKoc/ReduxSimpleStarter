373f5314474cf145d60402757a8c5d4dbf155510
emineKoc <eminekoc@gmail.com>
2016-04-19 15:29:40 -0400

    created the main files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// React library knows how to render components.
+import React from 'react'; // other way: const React = require('react')
+import ReactDOM from 'react-dom' // React DOM library knows how to render the JSX to DOM tree.
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar.js';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+const API_KEY = 'AIzaSyBW6oEaN0LZ5yskQduo0BPAm6cWhRAhG_c'; // youtube api
+// install an npm youtube-api-seach ** npm install --save youtube-api-search
+
+
+// Create a new component, which produce some html.
+const App = () => {  // const means this will never change! We can never not change the value of App.
+  return (
+    <div>Hi!</div>
+  )
+}
+
+// take this components generated html and put ont the DOM.
+ReactDOM.render(<App/>, document.querySelector('.container'));
